157490f0b3f0c69620cacf68f9cc9d4f2af6a66e
Jonas <merkel.joon@gmail.com>
2023-03-13 22:28:27 +0100

    reset

---
+++ dist/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'dist/index.html',
+    'dist/assets/info/contact.html',
+    'dist/assets/info/legal-notice.html',
+    'dist/assets/info/privacy.html',  
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
