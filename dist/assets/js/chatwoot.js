9e770e71f52d687e498bd318e01bf937a23cfd37
Jonas Merkel <merkel.joon@gmail.com>
2024-10-31 15:58:34 +0100

    Removed html tags in chatwoot.js

--- dist/assets/js/chatwoot.js
+++ dist/assets/js/chatwoot.js
@@ -1,16 +1,14 @@
-<script>
-  (function(d,t) {
-    var BASE_URL="https://chatwoot.netnas.de/";
-    var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-    g.src=BASE_URL+"/packs/js/sdk.js";
-    g.defer = true;
-    g.async = true;
-    s.parentNode.insertBefore(g,s);
-    g.onload=function(){
-      window.chatwootSDK.run({
-        websiteToken: 'fDz994VbeDZCLKJaZxGkDwQ6',
-        baseUrl: BASE_URL
-      })
-    }
-  })(document,"script");
-</script>
+(function (d, t) {
+  var BASE_URL = "https://chatwoot.netnas.de/";
+  var g = d.createElement(t), s = d.getElementsByTagName(t)[0];
+  g.src = BASE_URL + "/packs/js/sdk.js";
+  g.defer = true;
+  g.async = true;
+  s.parentNode.insertBefore(g, s);
+  g.onload = function () {
+    window.chatwootSDK.run({
+      websiteToken: 'fDz994VbeDZCLKJaZxGkDwQ6',
+      baseUrl: BASE_URL
+    })
+  }
+})(document, "script");
