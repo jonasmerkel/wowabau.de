081280fa7a12720f8a4380526c79ac30cf37f5ed
Jonas <87672485+jonasmerkel@users.noreply.github.com>
2024-10-31 15:48:06 +0100

    Added chatwoot.js script

---
+++ dist/assets/js/chatwoot.js
@@ -0,0 +1,16 @@
+<script>
+  (function(d,t) {
+    var BASE_URL="https://chatwoot.netnas.de/";
+    var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+    g.src=BASE_URL+"/packs/js/sdk.js";
+    g.defer = true;
+    g.async = true;
+    s.parentNode.insertBefore(g,s);
+    g.onload=function(){
+      window.chatwootSDK.run({
+        websiteToken: 'fDz994VbeDZCLKJaZxGkDwQ6',
+        baseUrl: BASE_URL
+      })
+    }
+  })(document,"script");
+</script>
